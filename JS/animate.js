3679dd21de41f9c70f6c667860705109f8440229
Oggi Danailov <ogidan@abv.bg>
2017-09-29 22:15:56 -0400

    animation added

---
+++ JS/animate.js
@@ -0,0 +1,11 @@
+$('.trumpet').addClass('animated fadeInDown');
+$('.cello').addClass('animated fadeInRight');
+$('.trombone').addClass('animated fadeInDown');
+
+$('.violin').addClass('animated fadeInUp');
+$('.tuba').addClass('animated fadeInRight');
+$('.drum').addClass('animated fadeInDown');
+
+$('.viola').addClass('animated fadeInUp');
+$('.french').addClass('animated fadeInRight');
+$('.piano').addClass('animated fadeInDown');

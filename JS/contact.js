98d4b7b046b6533094a84bb89d3597535ecf86c6
Oggi Danailov <ogidan@abv.bg>
2017-10-11 22:59:05 -0400

    parallax effect added

--- JS/contact.js
+++ JS/contact.js
@@ -1,18 +1,29 @@
 var elements = document.getElementsByClassName('modal')
 var secondLayer = document.getElementsByClassName('second-layer')[0]
+var map = document.getElementById('map')
 
 
 setTimeout(function(){
 	for(let i=0;i<elements.length;i++){
 
 	modals = elements[i]
-	console.log(modals)
+	
 	modals.style.opacity = 1;
+	map.style.opacity = 1;
 
 	}
 
 
 }, 1000)
+
+
+secondLayer.addEventListener('scroll', function(){
+	
+
+})
+
+
+
 
 
 
